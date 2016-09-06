5ba59d5b6529bfc28cb9d0eeffdb7e91577cce40
Stefan Horning <stefan@hornings.de>
2016-09-06 16:56:38 +0200

    Fix in test.yml and additioanl spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -9,10 +9,13 @@ describe "Logrotate setup" do
     it { should be_directory }
   end
 
+  global_max_size = ANSIBLE_VARS.fetch('logrotate_max_size', 'FAIL')
+
   ANSIBLE_VARS.fetch('logrotate_file_list', []).each do |file|
     describe file("/etc/logrotate.d/#{file['name']}") do
       it { should be_file }
-      its(:content) { should include(file['path'])}
+      its(:content) { should include(file['path']) }
+      its(:content) { should include("size #{ file.fetch('max_size', global_max_size) }") }
     end
   end
 end
